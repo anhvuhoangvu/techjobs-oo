9f847b8048d05f7167a57ff7e3f96a38472d63cd
Anh Vu <Anhvuhoangvu@gmail.com>
2018-12-14 02:59:35 +0700

    add func resolved

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -6,10 +6,9 @@ import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 /**
@@ -39,7 +38,7 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors, RedirectAttributes attributes) {
+    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
@@ -62,11 +61,11 @@ public class JobController {
         // add job to jobData
         jobData.add(newJob);
 
-        // add job id to redirect
-        attributes.addAttribute("id", newJob.getId());
+        // add job to model
+        model.addAttribute("job", newJob);
 
-        // display new job detail
-        return "redirect:/job";
+        // display new job detail
+        return "job-detail";
 
     }
 }
